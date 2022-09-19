8edb5d05e1f036fe132fbd42dbd664d61d56e9be
Hugo RECOLET <hrecolet@e1r11p17.clusters.42paris.fr>
2022-09-19 11:26:42 +0200

    [update]added base of socket programming and client_test, communication works but with one client and one message only

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 12:57:12 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/09/19 10:52:36 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/09/19 11:22:31 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,50 @@
 
 int main(int ac, char **av)
 {
-	int	serverFd;
-	int	newSocket;
-	int	valread;
-	int		opt = 1;
-	int		addrLen = sizeof(address);
+	if (ac > 0)
+	{
+		int	serverFd;
+		int	newSocket;
+		int	valread;
+		sockaddr_in	address;
+		int		opt = 1;
+		char	buffer[1024] = { 0 };
+		int		addrLen = sizeof(address);
 
-	// Creating socket file descriptor
-	serverFd = socket(AF_INET, SOCK_STREAM, 0);
+		// Creating socket file descriptor
+		serverFd = socket(AF_INET, SOCK_STREAM, 0);
+		if (!serverFd) {
+			perror("socket creation failed");
+			exit(EXIT_FAILURE);
+		}
+		if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0) {
+			perror("socket configuration failed");
+			exit(EXIT_FAILURE);
+		}
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_port = htons(PORT);
+
+		if (bind(serverFd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+			perror("Bind failed");
+			exit(EXIT_FAILURE);
+		}
+		if (listen(serverFd, 3) < 0) {
+			perror("listen failure");
+			exit(EXIT_FAILURE);
+		}
+		newSocket = accept(serverFd, (struct sockaddr*)&address, (socklen_t *)&addrLen);
+		if (newSocket < 0) {
+			perror("accept failure");
+			exit(EXIT_FAILURE);
+		}
+		valread = read(newSocket, buffer, 1024);
+		std::cout << buffer << std::endl;
+		send(newSocket, av[1], std::strlen(av[1]), 0);
+		std::cout << "message send" << std::endl;
+
+		close(newSocket);
+		shutdown(serverFd, SHUT_RDWR);
+	}
 }
 
--- webserv.hpp
+++ webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: hrecolet <hrecolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 10:52:17 by hrecolet          #+#    #+#             */
-/*   Updated: 2022/09/19 10:52:39 by hrecolet         ###   ########.fr       */
+/*   Updated: 2022/09/19 11:22:07 by hrecolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <iostream>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 #define PORT 8080
-
-struct	sockadrr_in	address;
